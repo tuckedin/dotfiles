04c6d3fb8a9f34318a570efd49e7fdac5dbeb03c
tuckedin <luke7272@hotmail.com>
2025-06-09 12:29:07 -0500

    comicMonoNF

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Comic Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "ComicMonoNF:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
